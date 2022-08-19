8f22872f0996ce904e29b0c64efb6c3d3541232c
Dmitrii Chechetkin <d.chechotkin@gmail.com>
2022-08-19 17:45:06 -0400

    Update DCPListener.java

--- app/src/main/java/com/couchbeans/DCPListener.java
+++ app/src/main/java/com/couchbeans/DCPListener.java
@@ -14,7 +14,7 @@ public class DCPListener {
         Client client = Client.builder()
                 .connectionString(env.getOrDefault("CBB_CLUSTER", "localhost"))
                 .credentials(
-                        env.getOrDefault("CBB_USER", "Administrator"),
+                        env.getOrDefault("CBB_USERNAME", "Administrator"),
                         env.getOrDefault("CBB_PASSWORD", "password")
                 )
                 .bucket(Couchbeans.BUCKET_NAME)
